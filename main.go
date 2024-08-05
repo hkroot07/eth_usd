0424f4113086d6c9fc7e71efd5b1050eecb665ea
H4XOR_ <hakroot07@gmail.com>
2024-08-05 22:41:28 +0200

    Create package plotter
    add function GeneratePlotFor ginerates a plot using data specified from CSV files using imported 'plot' packages.

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"eth_usd/csv_process"
+	"fmt"
+	"os"
+)
+
 func main() {
+	_, err := csv_process.LoadDataFrom("prices.csv")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading prices from CSV file: %v\n", err)
+		os.Exit(1)
+	}
 
 }
