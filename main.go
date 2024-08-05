f79a5bb59fc935edddda8774564cefab1e9ba17e
H4XOR_ <hakroot07@gmail.com>
2024-08-05 22:51:09 +0200

    Using the GeneratePlotFor function in main.go

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"eth_usd/csv_process"
+	"eth_usd/plotter"
 	"fmt"
 	"os"
+
+	"gonum.org/v1/plot/vg"
 )
 
 func main() {
-	_, err := csv_process.LoadDataFrom("prices.csv")
+	pricePairs, err := csv_process.LoadDataFrom("prices.csv")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading prices from CSV file: %v\n", err)
 		os.Exit(1)
 	}
 
+	pricesPlot, err := plotter.GeneratePlotFor(pricePairs)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error generating plot: %v\n", err)
+		os.Exit(1)
+	}
+	
+	if err := pricesPlot.Save(15*vg.Inch, 4*vg.Inch,"ethereum_prices.png")
 }
